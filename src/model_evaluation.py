f9feb643c394ee628ba8f1bc052dcd3f65458f8f
Shubham Mishra <shubham.smishra2004@gmail.com>
2026-05-28 15:48:36 +0530

    added dvclive to track experiments

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -5,6 +5,7 @@ import yaml
 import pandas as pd
 import numpy as np
 import pickle
+from dvclive import Live
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
 
 log_dir = 'logs'
@@ -108,7 +109,7 @@ def save_metrics(metrics: dict, file_path: str) -> None:
 
 def main():
     try:
-        
+        params = load_params(params_path='params.yaml')
         clf = load_model('./models/model.pkl')
         test_data = load_data('./data/processed/test_tfidf.csv')
         
@@ -116,6 +117,13 @@ def main():
         y_test = test_data.iloc[:, -1].values
 
         metrics = evaluate_model(clf, X_test, y_test)
+        
+        with Live(save_dvc_exp=True) as live:
+            live.log_metric('accuracy', accuracy_score(y_test, y_test))
+            live.log_metric('precision', precision_score(y_test, y_test))
+            live.log_metric('recall', recall_score(y_test, y_test))
+
+            live.log_params(params)
         
         save_metrics(metrics, 'reports/metrics.json')
     except Exception as e:
